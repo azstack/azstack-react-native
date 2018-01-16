b068a0719367a35585e23ef8fbe8f29265b56d48
Dat Dang <datdh@azstack.com>
2018-01-16 10:24:22 +0700

    conversation list listen on event group change admin

--- src/common/azstack/sdk/handler/event.js
+++ src/common/azstack/sdk/handler/event.js
@@ -322,6 +322,50 @@ class Event {
                 this.EventEmitter.emit(this.eventConstants.EVENT_NAME_ON_GROUP_RENAMED, { error: null, result: newMessage });
             }).catch((error) => { });
         };
+        this.AZStackCore.Delegates[this.AZStackCore.delegateConstants.DELEGATE_ON_GROUP_ADMIN_CHANGED] = (error, result) => {
+            if (error) {
+                this.EventEmitter.emit(this.eventConstants.EVENT_NAME_ON_GROUP_ADMIN_CHANGED, { error, result: null });
+                return;
+            }
+            let newMessage = result;
+            Promise.all([
+                new Promise((resolve, reject) => {
+                    this.AZStackCore.getUsersInformation({
+                        userIds: [newMessage.senderId]
+                    }).then((result) => {
+                        newMessage.sender = result.list[0];
+                        resolve(null);
+                    }).catch((error) => {
+                        newMessage.sender = { userId: newMessage.senderId };
+                        resolve(null);
+                    });
+                }),
+                new Promise((resolve, reject) => {
+                    this.AZStackCore.getDetailsGroup({
+                        groupId: newMessage.receiverId
+                    }).then((result) => {
+                        newMessage.receiver = result;
+                        resolve(null);
+                    }).catch((error) => {
+                        newMessage.receiver = { groupId: newMessage.receiverId };
+                        resolve(null);
+                    });
+                }),
+                new Promise((resolve, reject) => {
+                    this.AZStackCore.getUsersInformation({
+                        userIds: [newMessage.adminChanged.newAdminId]
+                    }).then((result) => {
+                        newMessage.adminChanged.newAdmin = result.list[0];
+                        resolve(null);
+                    }).catch((error) => {
+                        newMessage.adminChanged.newAdmin = { userId: newMessage.adminChanged.newAdminId }
+                        resolve(null);
+                    });
+                })
+            ]).then(() => {
+                this.EventEmitter.emit(this.eventConstants.EVENT_NAME_ON_GROUP_ADMIN_CHANGED, { error: null, result: newMessage });
+            }).catch((error) => { });
+        };
     };
 };
 
