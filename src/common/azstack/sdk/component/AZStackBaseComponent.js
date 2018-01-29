728c73c60afcdc6de350168fb3e0283751337a0d
Dat Dang <datdh@azstack.com>
2018-01-29 11:48:02 +0700

    video call component - not sending core instances through props

--- src/common/azstack/sdk/component/AZStackBaseComponent.js
+++ src/common/azstack/sdk/component/AZStackBaseComponent.js
@@ -162,11 +162,7 @@ export default class AZStackBaseComponent extends React.Component {
         return <VideoCallComponent
             key={key}
             Sizes={this.Sizes}
-            Language={this.Language}
-            CustomStyle={this.CustomStyle}
-            eventConstants={this.eventConstants}
-            AZStackCore={this.AZStackCore}
-            EventEmitter={this.EventEmitter}
+            getCoreInstances={this.getCoreInstances}
             {...options}
         />;
     };
