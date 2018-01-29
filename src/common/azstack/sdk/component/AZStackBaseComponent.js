2eaede029d097117ed8c0a10db8d76ad052eb945
Dat Dang <datdh@azstack.com>
2018-01-29 14:00:38 +0700

    dail pad component - not sending core instances through props

--- src/common/azstack/sdk/component/AZStackBaseComponent.js
+++ src/common/azstack/sdk/component/AZStackBaseComponent.js
@@ -145,11 +145,7 @@ export default class AZStackBaseComponent extends React.Component {
         return <NumberPadComponent
             key={key}
             Sizes={this.Sizes}
-            Language={this.Language}
-            CustomStyle={this.CustomStyle}
-            eventConstants={this.eventConstants}
-            AZStackCore={this.AZStackCore}
-            EventEmitter={this.EventEmitter}
+            getCoreInstances={this.getCoreInstances}
             {...options}
         />;
     };
--- src/common/azstack/sdk/component/NumberPadComponent.js
+++ src/common/azstack/sdk/component/NumberPadComponent.js
@@ -8,7 +8,7 @@ import {
 	Dimensions,
 	TouchableOpacity,
 	FlatList,
-	StatusBar, 
+	StatusBar,
 	Platform,
 	Alert,
 	TextInput,
@@ -17,9 +17,6 @@ import {
 
 import ScreenBlockComponent from './part/screen/ScreenBlockComponent';
 import ScreenHeaderBlockComponent from './part/screen/ScreenHeaderBlockComponent';
-import ScreenBodyBlockComponent from './part/screen/ScreenBodyBlockComponent';
-import EmptyBlockComponent from './part/common/EmptyBlockComponent';
-import SearchBlockComponent from './part/common/SearchBlockComponent';
 
 const { height, width } = Dimensions.get('window');
 
@@ -30,6 +27,7 @@ const ic_input_back = require('../static/image/ic_input_back.png');
 class NumberPadComponent extends React.Component {
 	constructor(props) {
 		super(props);
+		this.coreInstances = props.getCoreInstances();
 		this.state = {
 			phoneNumber: '',
 			onCall: null,
@@ -38,11 +36,11 @@ class NumberPadComponent extends React.Component {
 	}
 
 	onClickNumber(number) {
-		this.setState({phoneNumber: this.state.phoneNumber + number});
+		this.setState({ phoneNumber: this.state.phoneNumber + number });
 	}
 
 	onClear() {
-		this.setState({phoneNumber: this.state.phoneNumber.slice(0,  -1)});
+		this.setState({ phoneNumber: this.state.phoneNumber.slice(0, -1) });
 	}
 
 	onCall() {
@@ -57,125 +55,125 @@ class NumberPadComponent extends React.Component {
 
 	render() {
 		return (
-            <ScreenBlockComponent
+			<ScreenBlockComponent
 				fullScreen={false}
-				CustomStyle={this.props.CustomStyle}
+				getCoreInstances={this.props.getCoreInstances}
 				style={this.props.style}
-            >
+			>
 				{this.props.header !== 'hidden' && <ScreenHeaderBlockComponent
-					CustomStyle={this.props.CustomStyle}
+					getCoreInstances={this.props.getCoreInstances}
 					onBackButtonPressed={() => this.props.onBackButtonPressed()}
 					title={"Callout"}
 				/>}
-				<View style={{backgroundColor: '#fff', justifyContent: 'flex-end', alignItems: 'center', flex: 1, paddingBottom: 40}}>
-					<View style={{width: 270, alignSelf: 'center', flexDirection: 'row', justifyContent: 'flex-end', backgroundColor: '#fff', alignItems: 'center', height: 50, marginBottom: 20}}>
-						<View style={{ alignItems: 'flex-end'}}>
-							<Text style={{fontSize: this.state.phoneNumber.length <= 10 ? 40 : this.state.phoneNumber.length <= 13 ? 30 : 20 }}>{this.state.phoneNumber}</Text>
+				<View style={{ backgroundColor: '#fff', justifyContent: 'flex-end', alignItems: 'center', flex: 1, paddingBottom: 40 }}>
+					<View style={{ width: 270, alignSelf: 'center', flexDirection: 'row', justifyContent: 'flex-end', backgroundColor: '#fff', alignItems: 'center', height: 50, marginBottom: 20 }}>
+						<View style={{ alignItems: 'flex-end' }}>
+							<Text style={{ fontSize: this.state.phoneNumber.length <= 10 ? 40 : this.state.phoneNumber.length <= 13 ? 30 : 20 }}>{this.state.phoneNumber}</Text>
 						</View>
 						{
 							this.state.phoneNumber != '' && <TouchableOpacity onPress={() => this.onClear()}>
-								<View style={{justifyContent: 'center', alignItems: 'flex-end', width: 40, height: 50}}>
-									<Image source={ic_input_back} style={{width: 25, height: 19}} />
+								<View style={{ justifyContent: 'center', alignItems: 'flex-end', width: 40, height: 50 }}>
+									<Image source={ic_input_back} style={{ width: 25, height: 19 }} />
 								</View>
 							</TouchableOpacity>
 						}
 					</View>
-					<View style={{width: width, justifyContent: 'center', alignItems: 'center'}}>
-						<View style={{width: 270, alignSelf: 'center'}}>
-							<View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-								<TouchableOpacity onPress={() => {this.onClickNumber('1')}}>
+					<View style={{ width: width, justifyContent: 'center', alignItems: 'center' }}>
+						<View style={{ width: 270, alignSelf: 'center' }}>
+							<View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+								<TouchableOpacity onPress={() => { this.onClickNumber('1') }}>
 									<View style={styles.number}>
-										<Text style={{fontSize: 34}}>1</Text>
-										<Text style={{fontSize: 10}}> </Text>
+										<Text style={{ fontSize: 34 }}>1</Text>
+										<Text style={{ fontSize: 10 }}> </Text>
 									</View>
 								</TouchableOpacity>
-								<TouchableOpacity onPress={() => {this.onClickNumber('2')}}>
+								<TouchableOpacity onPress={() => { this.onClickNumber('2') }}>
 									<View style={styles.number}>
-										<Text style={{fontSize: 34}}>2</Text>
-										<Text style={{fontSize: 10}}>A B C</Text>
+										<Text style={{ fontSize: 34 }}>2</Text>
+										<Text style={{ fontSize: 10 }}>A B C</Text>
 									</View>
 								</TouchableOpacity>
-								<TouchableOpacity onPress={() => {this.onClickNumber('3')}}>
-									<View style={[styles.number, {marginRight: 0}]}>
-										<Text style={{fontSize: 34}}>3</Text>
-										<Text style={{fontSize: 10}}>D E F</Text>
+								<TouchableOpacity onPress={() => { this.onClickNumber('3') }}>
+									<View style={[styles.number, { marginRight: 0 }]}>
+										<Text style={{ fontSize: 34 }}>3</Text>
+										<Text style={{ fontSize: 10 }}>D E F</Text>
 									</View>
 								</TouchableOpacity>
 							</View>
-							<View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-								<TouchableOpacity onPress={() => {this.onClickNumber('4')}}>
+							<View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+								<TouchableOpacity onPress={() => { this.onClickNumber('4') }}>
 									<View style={styles.number}>
-										<Text style={{fontSize: 34}}>4</Text>
-										<Text style={{fontSize: 10}}>G H I</Text>
+										<Text style={{ fontSize: 34 }}>4</Text>
+										<Text style={{ fontSize: 10 }}>G H I</Text>
 									</View>
 								</TouchableOpacity>
-								<TouchableOpacity onPress={() => {this.onClickNumber('5')}}>
+								<TouchableOpacity onPress={() => { this.onClickNumber('5') }}>
 									<View style={styles.number}>
-										<Text style={{fontSize: 34}}>5</Text>
-										<Text style={{fontSize: 10}}>J K L</Text>
+										<Text style={{ fontSize: 34 }}>5</Text>
+										<Text style={{ fontSize: 10 }}>J K L</Text>
 									</View>
 								</TouchableOpacity>
-								<TouchableOpacity onPress={() => {this.onClickNumber('6')}}>
-									<View style={[styles.number, {marginRight: 0}]}>
-										<Text style={{fontSize: 34}}>6</Text>
-										<Text style={{fontSize: 10}}>M N O</Text>
+								<TouchableOpacity onPress={() => { this.onClickNumber('6') }}>
+									<View style={[styles.number, { marginRight: 0 }]}>
+										<Text style={{ fontSize: 34 }}>6</Text>
+										<Text style={{ fontSize: 10 }}>M N O</Text>
 									</View>
 								</TouchableOpacity>
 							</View>
-							<View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-								<TouchableOpacity onPress={() => {this.onClickNumber('7')}}>
+							<View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+								<TouchableOpacity onPress={() => { this.onClickNumber('7') }}>
 									<View style={styles.number}>
-										<Text style={{fontSize: 34}}>7</Text>
-										<Text style={{fontSize: 10}}>P Q R S</Text>
+										<Text style={{ fontSize: 34 }}>7</Text>
+										<Text style={{ fontSize: 10 }}>P Q R S</Text>
 									</View>
 								</TouchableOpacity>
-								<TouchableOpacity onPress={() => {this.onClickNumber('8')}}>
+								<TouchableOpacity onPress={() => { this.onClickNumber('8') }}>
 									<View style={styles.number}>
-										<Text style={{fontSize: 34}}>8</Text>
-										<Text style={{fontSize: 10}}>T U V</Text>
+										<Text style={{ fontSize: 34 }}>8</Text>
+										<Text style={{ fontSize: 10 }}>T U V</Text>
 									</View>
 								</TouchableOpacity>
-								<TouchableOpacity onPress={() => {this.onClickNumber('9')}}>
-									<View style={[styles.number, {marginRight: 0}]}>
-										<Text style={{fontSize: 34}}>9</Text>
-										<Text style={{fontSize: 10}}>W X Y Z</Text>
+								<TouchableOpacity onPress={() => { this.onClickNumber('9') }}>
+									<View style={[styles.number, { marginRight: 0 }]}>
+										<Text style={{ fontSize: 34 }}>9</Text>
+										<Text style={{ fontSize: 10 }}>W X Y Z</Text>
 									</View>
 								</TouchableOpacity>
 							</View>
-							<View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-								<TouchableOpacity onPress={() => {this.onClickNumber('*')}}>
+							<View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+								<TouchableOpacity onPress={() => { this.onClickNumber('*') }}>
 									<View style={styles.number}>
-										<Text style={{fontSize: 34}}>*</Text>
-										<Text style={{fontSize: 10}}> </Text>
+										<Text style={{ fontSize: 34 }}>*</Text>
+										<Text style={{ fontSize: 10 }}> </Text>
 									</View>
 								</TouchableOpacity>
-								<TouchableOpacity onPress={() => {this.onClickNumber('0')}} onLongPress={() => this.onClickNumber('+')}>
+								<TouchableOpacity onPress={() => { this.onClickNumber('0') }} onLongPress={() => this.onClickNumber('+')}>
 									<View style={styles.number}>
-										<Text style={{fontSize: 34}}>0</Text>
-										<Text style={{fontSize: 10}}>+</Text>
+										<Text style={{ fontSize: 34 }}>0</Text>
+										<Text style={{ fontSize: 10 }}>+</Text>
 									</View>
 								</TouchableOpacity>
-								<TouchableOpacity onPress={() => {this.onClickNumber('#')}}>
-									<View style={[styles.number, {marginRight: 0}]}>
-										<Text style={{fontSize: 34}}>#</Text>
-										<Text style={{fontSize: 10}}> </Text>
+								<TouchableOpacity onPress={() => { this.onClickNumber('#') }}>
+									<View style={[styles.number, { marginRight: 0 }]}>
+										<Text style={{ fontSize: 34 }}>#</Text>
+										<Text style={{ fontSize: 10 }}> </Text>
 									</View>
 								</TouchableOpacity>
 							</View>
-							<View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+							<View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
 								<TouchableOpacity onPress={() => this.onCall()}>
-									<View style={[styles.number, {backgroundColor: '#44f441', marginRight: 0}]}>
-										<Image source={ic_action_answer} style={{width: 40, height: 40}} />
+									<View style={[styles.number, { backgroundColor: '#44f441', marginRight: 0 }]}>
+										<Image source={ic_action_answer} style={{ width: 40, height: 40 }} />
 									</View>
-								</TouchableOpacity>					
+								</TouchableOpacity>
 							</View>
 						</View>
 					</View>
 				</View>
-                {
-                    this.state.onCall
-                }
-            </ScreenBlockComponent>
+				{
+					this.state.onCall
+				}
+			</ScreenBlockComponent>
 		);
 	}
 }
@@ -185,12 +183,12 @@ export default NumberPadComponent;
 
 const styles = {
 	number: {
-		backgroundColor: '#f0f0f0', 
-		width: 70, 
-		height: 70, 
-		borderRadius: 35, 
-		justifyContent: 'center', 
-		alignItems: 'center', 
+		backgroundColor: '#f0f0f0',
+		width: 70,
+		height: 70,
+		borderRadius: 35,
+		justifyContent: 'center',
+		alignItems: 'center',
 		marginRight: 10,
 		marginTop: 5,
 		marginBottom: 5,
