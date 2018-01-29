01409c4227df6bfce41c69d0109635940b9691d6
Dat Dang <datdh@azstack.com>
2018-01-29 13:45:12 +0700

    contact component - not sending core instances through props

--- src/common/azstack/sdk/component/AZStackBaseComponent.js
+++ src/common/azstack/sdk/component/AZStackBaseComponent.js
@@ -136,11 +136,7 @@ export default class AZStackBaseComponent extends React.Component {
         return <ContactComponent
             key={key}
             Sizes={this.Sizes}
-            Language={this.Language}
-            CustomStyle={this.CustomStyle}
-            eventConstants={this.eventConstants}
-            AZStackCore={this.AZStackCore}
-            EventEmitter={this.EventEmitter}
+            getCoreInstances={this.getCoreInstances}
             {...options}
         />;
     };
