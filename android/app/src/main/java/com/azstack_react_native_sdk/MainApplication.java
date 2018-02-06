e6dbe7f9ac5fbb1635bd6710c3a5b2c82bbf6aec
Dat Dang <datdh.js@gmail.com>
2018-02-06 16:54:52 +0700

    done send file

--- android/app/src/main/java/com/azstack_react_native_sdk/MainApplication.java
+++ android/app/src/main/java/com/azstack_react_native_sdk/MainApplication.java
@@ -3,6 +3,7 @@ package com.azstack_react_native_sdk;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativedocumentpicker.ReactNativeDocumentPicker;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeDocumentPicker(),
             new PickerPackage(),
           new WebRTCModulePackage()
       );
--- src/common/azstack/sdk/component/part/chat/ChatInputComponentBlock.js
+++ src/common/azstack/sdk/component/part/chat/ChatInputComponentBlock.js
@@ -9,6 +9,7 @@ import {
     Text
 } from 'react-native';
 import ImagePicker from 'react-native-image-crop-picker';
+import { DocumentPicker, DocumentPickerUtil } from 'react-native-document-picker';
 
 class ChatInputComponentBlock extends React.Component {
     constructor(props) {
@@ -93,6 +94,87 @@ class ChatInputComponentBlock extends React.Component {
         for (let subscriptionName in this.subscriptions) {
             this.subscriptions[subscriptionName].remove();
         }
+    };
+
+    detectFileType(mimeType) {
+
+        let mimeTypes = {
+            word: [
+                'application/msword',
+                'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                'application/vnd.openxmlformats-officedocument.wordprocessingml.template',
+                'application/vnd.ms-word.document.macroEnabled.12',
+                'application/vnd.ms-word.template.macroEnabled.12'
+            ],
+            excel: [
+                'application/vnd.ms-excel',
+                'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                'application/vnd.openxmlformats-officedocument.spreadsheetml.template',
+                'application/vnd.ms-excel.sheet.macroEnabled.12',
+                'application/vnd.ms-excel.template.macroEnabled.12',
+                'application/vnd.ms-excel.addin.macroEnabled.12',
+                'application/vnd.ms-excel.sheet.binary.macroEnabled.12'
+            ],
+            powerpoint: [
+                'application/vnd.ms-powerpoint',
+                'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+                'application/vnd.openxmlformats-officedocument.presentationml.template',
+                'application/vnd.openxmlformats-officedocument.presentationml.slideshow',
+                'application/vnd.ms-powerpoint.addin.macroEnabled.12',
+                'application/vnd.ms-powerpoint.presentation.macroEnabled.12',
+                'application/vnd.ms-powerpoint.template.macroEnabled.12',
+                'application/vnd.ms-powerpoint.slideshow.macroEnabled.12'
+            ],
+            pdf: [
+                'application/pdf'
+            ],
+            plaintext: [
+                'text/plain',
+                'text/csv'
+            ],
+            archive: [
+                'application/x-tar',
+                'application/gzip',
+                'application/x-7z-compressed',
+                'application/vnd.android.package-archive',
+                'application/x-apple-diskimage',
+                'application/x-rar-compressed',
+                'application/x-gtar',
+                'application/zip'
+            ]
+        }
+
+        if (mimeType.indexOf('image') > -1) {
+            return this.coreInstances.AZStackCore.chatConstants.MESSAGE_FILE_TYPE_IMAGE;
+        }
+        if (mimeType.indexOf('audio') > -1) {
+            return this.coreInstances.AZStackCore.chatConstants.MESSAGE_FILE_TYPE_AUDIO;
+        }
+        if (mimeType.indexOf('video') > -1) {
+            return this.coreInstances.AZStackCore.chatConstants.MESSAGE_FILE_TYPE_VIDEO;
+        }
+        if (mimeTypes.excel.indexOf(mimeType) > -1) {
+            return this.coreInstances.AZStackCore.chatConstants.MESSAGE_FILE_TYPE_EXCEL;
+        }
+        if (mimeTypes.word.indexOf(mimeType) > -1) {
+            return this.coreInstances.AZStackCore.chatConstants.MESSAGE_FILE_TYPE_WORD;
+        }
+        if (mimeTypes.powerpoint.indexOf(mimeType) > -1) {
+            return this.coreInstances.AZStackCore.chatConstants.MESSAGE_FILE_TYPE_POWERPOINT;
+        }
+        if (mimeTypes.pdf.indexOf(mimeType) > -1) {
+            return this.coreInstances.AZStackCore.chatConstants.MESSAGE_FILE_TYPE_PDF;
+        }
+        if (mimeTypes.plaintext.indexOf(mimeType) > -1) {
+            return this.coreInstances.AZStackCore.chatConstants.MESSAGE_FILE_TYPE_TEXT;
+        }
+        if (mimeType.indexOf('text') > -1) {
+            return this.coreInstances.AZStackCore.chatConstants.MESSAGE_FILE_TYPE_CODE;
+        }
+        if (mimeTypes.archive.indexOf(mimeType) > -1) {
+            return this.coreInstances.AZStackCore.chatConstants.MESSAGE_FILE_TYPE_ARCHIVE;
+        }
+        return this.coreInstances.AZStackCore.chatConstants.MESSAGE_FILE_TYPE_UNKNOWN;
     };
 
     onMessageStatusChanged(myMessage) {
@@ -513,7 +595,124 @@ class ChatInputComponentBlock extends React.Component {
 
         }).catch((error) => { });
     };
-    onFileBoxOptionFileButtonPressed() { };
+    onFileBoxOptionFileButtonPressed() {
+        DocumentPicker.show({
+            filetype: [DocumentPickerUtil.allFiles()],
+        }, (error, selectedFile) => {
+            if (error) {
+                return;
+            }
+
+            if (!this.coreInstances.AZStackCore.slaveSocketConnected) {
+                Alert.alert(
+                    this.coreInstances.Language.getText('ALERT_TITLE_ERROR_TEXT'),
+                    this.coreInstances.Language.getText('CHAT_INPUT_SEND_MESSAGE_ERROR_TEXT'),
+                    [
+                        { text: this.coreInstances.Language.getText('ALERT_BUTTON_TITLE_OK_TEXT'), onPress: () => { } }
+                    ],
+                    { cancelable: true }
+                );
+            }
+
+            if (selectedFile.fileSize > this.coreInstances.limitConstants.LIMIT_MAX_FILE_SIZE) {
+                Alert.alert(
+                    this.coreInstances.Language.getText('ALERT_TITLE_ERROR_TEXT'),
+                    `${this.coreInstances.Language.getText('CHAT_INPUT_FILE_SIZE_TOO_BIG_ERROR_TEXT')} ${this.coreInstances.FileConverter.sizeAsString(this.coreInstances.limitConstants.LIMIT_MAX_FILE_SIZE, true)}`,
+                    [
+                        { text: this.coreInstances.Language.getText('ALERT_BUTTON_TITLE_OK_TEXT'), onPress: () => { } }
+                    ],
+                    { cancelable: true }
+                );
+                return;
+            }
+
+            let currentTime = new Date().getTime();
+            this.coreInstances.AZStackCore.newUniqueId();
+            sendingFile = {
+                chatType: this.props.chatType,
+                chatId: this.props.chatId,
+                senderId: this.coreInstances.AZStackCore.authenticatedUser.userId,
+                sender: this.coreInstances.AZStackCore.authenticatedUser,
+                receiverId: this.props.chatId,
+                receiver: this.props.chatTarget,
+                msgId: this.coreInstances.AZStackCore.uniqueId,
+                type: this.coreInstances.AZStackCore.chatConstants.MESSAGE_TYPE_FILE,
+                status: this.coreInstances.AZStackCore.chatConstants.MESSAGE_STATUS_SENDING,
+                deleted: this.coreInstances.AZStackCore.chatConstants.MESSAGE_DELETED_FALSE,
+                created: currentTime,
+                modified: currentTime,
+                file: {
+                    name: selectedFile.fileName,
+                    length: selectedFile.fileSize,
+                    type: this.detectFileType(selectedFile.type),
+                    url: selectedFile.uri,
+                    mimeType: selectedFile.type
+                }
+            };
+
+            Promise.all([
+                new Promise((resolve, reject) => {
+                    switch (sendingFile.file.type) {
+                        case this.coreInstances.AZStackCore.chatConstants.MESSAGE_FILE_TYPE_IMAGE:
+                            Image.getSize(myMessage.file.url, (width, height) => {
+                                sendingFile.file.width = width;
+                                sendingFile.file.height = height;
+                                resolve(null);
+                            }, (error) => {
+                                resolve(null);
+                            });
+                            break;
+                        case this.coreInstances.AZStackCore.chatConstants.MESSAGE_FILE_TYPE_AUDIO:
+                        case this.coreInstances.AZStackCore.chatConstants.MESSAGE_FILE_TYPE_VIDEO:
+                        case this.coreInstances.AZStackCore.chatConstants.MESSAGE_FILE_TYPE_EXCEL:
+                        case this.coreInstances.AZStackCore.chatConstants.MESSAGE_FILE_TYPE_WORD:
+                        case this.coreInstances.AZStackCore.chatConstants.MESSAGE_FILE_TYPE_POWERPOINT:
+                        case this.coreInstances.AZStackCore.chatConstants.MESSAGE_FILE_TYPE_PDF:
+                        case this.coreInstances.AZStackCore.chatConstants.MESSAGE_FILE_TYPE_TEXT:
+                        case this.coreInstances.AZStackCore.chatConstants.MESSAGE_FILE_TYPE_CODE:
+                        case this.coreInstances.AZStackCore.chatConstants.MESSAGE_FILE_TYPE_ARCHIVE:
+                        case this.coreInstances.AZStackCore.chatConstants.MESSAGE_FILE_TYPE_UNKNOWN:
+                        default:
+                            resolve(null);
+                            break;
+                    }
+                })
+            ]).then(() => {
+                this.coreInstances.EventEmitter.emit(this.coreInstances.eventConstants.EVENT_NAME_ON_NEW_MESSAGE_RETURN, { error: null, result: { ...sendingFile } });
+
+                Promise.all([
+                    new Promise((resolve, reject) => {
+                        const data = new FormData();
+                        data.append('uid', this.coreInstances.AZStackCore.authenticatedUser.azStackUserId);
+                        data.append('msgId', sendingFile.msgId);
+                        data.append('hash', this.coreInstances.FileConverter.MD5(`${sendingFile.msgId}abc_d123_##$$${this.coreInstances.AZStackCore.authenticatedUser.azStackUserId}`))
+                        data.append('fileToUpload', {
+                            uri: sendingFile.file.url,
+                            type: sendingFile.file.mimeType,
+                            name: sendingFile.file.name
+                        });
+                        fetch(this.coreInstances.linkConstants.LINK_API_URL_UPLOAD_FILE, {
+                            method: 'post',
+                            body: data
+                        }).then((response) => response.json()).then((responseJson) => {
+                            if (responseJson.status === 1000) {
+                                sendingFile.file.url = responseJson.data.replace('http://', 'https://');
+                            } else {
+                                sendingFile.status = -1;
+                            }
+                            resolve(null);
+                        }).catch((error) => {
+                            sendingFile.status = -1;
+                            resolve(null);
+                        });;
+                    })
+                ]).then(() => {
+                    this.sendFileMessage([sendingFile]);
+                }).catch((error) => { });
+
+            }).catch(() => { });
+        });
+    };
     onFileBoxOptionLocationButtonPressed() { };
     onFileBoxOptionVoiceButtonPressed() { };
     onFileBoxOptionDrawingButtonPressed() { };
