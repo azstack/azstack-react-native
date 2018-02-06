85eaf1d152637b0b14f8c2a959d3703ddfa94a78
Dat Dang <datdh.js@gmail.com>
2018-02-06 15:00:09 +0700

    done send image from camera

--- src/common/azstack/sdk/component/part/chat/ChatInputComponentBlock.js
+++ src/common/azstack/sdk/component/part/chat/ChatInputComponentBlock.js
@@ -428,7 +428,91 @@ class ChatInputComponentBlock extends React.Component {
             );
         });
     };
-    onFileBoxOptionCameraButtonPressed() { };
+    onFileBoxOptionCameraButtonPressed() {
+        ImagePicker.openCamera({}).then((capturedImage) => {
+            if (!this.coreInstances.AZStackCore.slaveSocketConnected) {
+                Alert.alert(
+                    this.coreInstances.Language.getText('ALERT_TITLE_ERROR_TEXT'),
+                    this.coreInstances.Language.getText('CHAT_INPUT_SEND_MESSAGE_ERROR_TEXT'),
+                    [
+                        { text: this.coreInstances.Language.getText('ALERT_BUTTON_TITLE_OK_TEXT'), onPress: () => { } }
+                    ],
+                    { cancelable: true }
+                );
+            }
+
+            if (capturedImage.size > this.coreInstances.limitConstants.LIMIT_MAX_FILE_SIZE) {
+                Alert.alert(
+                    this.coreInstances.Language.getText('ALERT_TITLE_ERROR_TEXT'),
+                    `${this.coreInstances.Language.getText('CHAT_INPUT_FILE_SIZE_TOO_BIG_ERROR_TEXT')} ${this.coreInstances.FileConverter.sizeAsString(this.coreInstances.limitConstants.LIMIT_MAX_FILE_SIZE, true)}`,
+                    [
+                        { text: this.coreInstances.Language.getText('ALERT_BUTTON_TITLE_OK_TEXT'), onPress: () => { } }
+                    ],
+                    { cancelable: true }
+                );
+                return;
+            }
+
+            let currentTime = new Date().getTime();
+            this.coreInstances.AZStackCore.newUniqueId();
+            sendingImage = {
+                chatType: this.props.chatType,
+                chatId: this.props.chatId,
+                senderId: this.coreInstances.AZStackCore.authenticatedUser.userId,
+                sender: this.coreInstances.AZStackCore.authenticatedUser,
+                receiverId: this.props.chatId,
+                receiver: this.props.chatTarget,
+                msgId: this.coreInstances.AZStackCore.uniqueId,
+                type: this.coreInstances.AZStackCore.chatConstants.MESSAGE_TYPE_FILE,
+                status: this.coreInstances.AZStackCore.chatConstants.MESSAGE_STATUS_SENDING,
+                deleted: this.coreInstances.AZStackCore.chatConstants.MESSAGE_DELETED_FALSE,
+                created: currentTime,
+                modified: currentTime,
+                file: {
+                    name: this.coreInstances.FileConverter.nameFromPath(capturedImage.path),
+                    length: capturedImage.size,
+                    type: this.coreInstances.AZStackCore.chatConstants.MESSAGE_FILE_TYPE_IMAGE,
+                    url: capturedImage.path,
+                    width: capturedImage.width,
+                    height: capturedImage.height,
+                    mimeType: capturedImage.mime
+                }
+            };
+
+            this.coreInstances.EventEmitter.emit(this.coreInstances.eventConstants.EVENT_NAME_ON_NEW_MESSAGE_RETURN, { error: null, result: { ...sendingImage } });
+
+            Promise.all([
+                new Promise((resolve, reject) => {
+                    const data = new FormData();
+                    data.append('uid', this.coreInstances.AZStackCore.authenticatedUser.azStackUserId);
+                    data.append('msgId', sendingImage.msgId);
+                    data.append('hash', this.coreInstances.FileConverter.MD5(`${sendingImage.msgId}abc_d123_##$$${this.coreInstances.AZStackCore.authenticatedUser.azStackUserId}`))
+                    data.append('fileToUpload', {
+                        uri: sendingImage.file.url,
+                        type: sendingImage.file.mimeType,
+                        name: sendingImage.file.name
+                    });
+                    fetch(this.coreInstances.linkConstants.LINK_API_URL_UPLOAD_FILE, {
+                        method: 'post',
+                        body: data
+                    }).then((response) => response.json()).then((responseJson) => {
+                        if (responseJson.status === 1000) {
+                            sendingImage.file.url = responseJson.data.replace('http://', 'https://');
+                        } else {
+                            sendingImage.status = -1;
+                        }
+                        resolve(null);
+                    }).catch((error) => {
+                        sendingImage.status = -1;
+                        resolve(null);
+                    });;
+                })
+            ]).then(() => {
+                this.sendFileMessage([sendingImage]);
+            }).catch((error) => { });
+
+        }).catch((error) => { });
+    };
     onFileBoxOptionFileButtonPressed() { };
     onFileBoxOptionLocationButtonPressed() { };
     onFileBoxOptionVoiceButtonPressed() { };
