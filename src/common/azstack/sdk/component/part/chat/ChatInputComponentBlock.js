29869dd5674d72e4b3658101e1c374d6d1b14a1d
Dat Dang <datdh.js@gmail.com>
2018-02-12 15:46:29 +0700

    no message

--- src/common/azstack/sdk/component/part/chat/ChatInputComponentBlock.js
+++ src/common/azstack/sdk/component/part/chat/ChatInputComponentBlock.js
@@ -299,67 +299,27 @@ class ChatInputComponentBlock extends React.Component {
             text: textMessage.text
         }).then((result) => { }).catch((error) => { });
     };
-    sendStickerMessage(event) {
+    sendStickerMessage(stickerMessage) {
+        this.coreInstances.EventEmitter.emit(this.coreInstances.eventConstants.EVENT_NAME_ON_NEW_MESSAGE_RETURN, { error: null, result: { ...stickerMessage } });
 
-        if (!this.coreInstances.AZStackCore.slaveSocketConnected) {
-            Alert.alert(
-                this.coreInstances.Language.getText('ALERT_TITLE_ERROR_TEXT'),
-                this.coreInstances.Language.getText('CHAT_INPUT_SEND_MESSAGE_ERROR_TEXT'),
-                [
-                    { text: this.coreInstances.Language.getText('ALERT_BUTTON_TITLE_OK_TEXT'), onPress: () => { } }
-                ],
-                { cancelable: true }
-            );
-            return;
-        }
-
-        Image.getSize(`${this.coreInstances.linkConstants.LINK_API_URL_STICKER}${event.catId}/${event.name}`, (width, height) => {
-            let currentTime = new Date().getTime();
-            this.coreInstances.AZStackCore.newUniqueId();
-            let stickerMessage = {
-                chatType: this.props.chatType,
-                chatId: this.props.chatId,
-                senderId: this.coreInstances.AZStackCore.authenticatedUser.userId,
-                sender: this.coreInstances.AZStackCore.authenticatedUser,
-                receiverId: this.props.chatId,
-                receiver: this.props.chatTarget,
-                msgId: this.coreInstances.AZStackCore.uniqueId,
-                type: this.coreInstances.AZStackCore.chatConstants.MESSAGE_TYPE_STICKER,
-                status: this.coreInstances.AZStackCore.chatConstants.MESSAGE_STATUS_SENDING,
-                deleted: this.coreInstances.AZStackCore.chatConstants.MESSAGE_DELETED_FALSE,
-                created: currentTime,
-                modified: currentTime,
-                sticker: {
-                    name: event.name,
-                    catId: event.catId,
-                    url: `${this.coreInstances.linkConstants.LINK_API_URL_STICKER}${event.catId}/${event.name}`,
-                    width: width,
-                    height: height
-                }
-            };
+        this.sendingMessageFailChecks[stickerMessage.msgId] = setTimeout(() => {
+            stickerMessage.status = -1;
             this.coreInstances.EventEmitter.emit(this.coreInstances.eventConstants.EVENT_NAME_ON_NEW_MESSAGE_RETURN, { error: null, result: { ...stickerMessage } });
+            delete this.sendingMessageFailChecks[stickerMessage.msgId];
+        }, 5000);
 
-            this.sendingMessageFailChecks[stickerMessage.msgId] = setTimeout(() => {
-                stickerMessage.status = -1;
-                this.coreInstances.EventEmitter.emit(this.coreInstances.eventConstants.EVENT_NAME_ON_NEW_MESSAGE_RETURN, { error: null, result: { ...stickerMessage } });
-                delete this.sendingMessageFailChecks[stickerMessage.msgId];
-            }, 5000);
-
-            this.coreInstances.AZStackCore.newMessage({
-                chatType: this.props.chatType,
-                chatId: this.props.chatId,
-                msgId: stickerMessage.msgId,
-                sticker: {
-                    name: stickerMessage.sticker.name,
-                    catId: stickerMessage.sticker.catId,
-                    url: stickerMessage.sticker.url,
-                    width: stickerMessage.sticker.width,
-                    height: stickerMessage.sticker.height
-                }
-            }).then((result) => { }).catch((error) => { });
-        }, (error) => {
-
-        });
+        this.coreInstances.AZStackCore.newMessage({
+            chatType: this.props.chatType,
+            chatId: this.props.chatId,
+            msgId: stickerMessage.msgId,
+            sticker: {
+                name: stickerMessage.sticker.name,
+                catId: stickerMessage.sticker.catId,
+                url: stickerMessage.sticker.url,
+                width: stickerMessage.sticker.width,
+                height: stickerMessage.sticker.height
+            }
+        }).then((result) => { }).catch((error) => { });
     };
     sendFileMessage(fileMessages) {
         fileMessages.map((fileMessage) => {
@@ -779,7 +739,7 @@ class ChatInputComponentBlock extends React.Component {
                         <ChatInputStickerBlockComponent
                             getCoreInstances={this.props.getCoreInstances}
                             onCloseButtonPressed={this.closeStickerBox}
-                            onStickerPressed={this.sendStickerMessage}
+                            onStickerMessageGenerated={this.sendStickerMessage}
                         />
                     )
                 }
--- src/common/azstack/sdk/component/part/chat/ChatInputStickerBlockComponent.js
+++ src/common/azstack/sdk/component/part/chat/ChatInputStickerBlockComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+    Alert,
     View,
     Image,
     TouchableOpacity,
@@ -36,10 +37,53 @@ class ChatInputStickerBlockComponent extends React.Component {
         };
 
         this.changeStickerTab = this.changeStickerTab.bind(this);
+        this.onStickerPressed = this.onStickerPressed.bind(this);
     };
 
     changeStickerTab() {
         this.setState({ selected: index });
+    };
+    onStickerPressed(event) {
+        if (!this.coreInstances.AZStackCore.slaveSocketConnected) {
+            Alert.alert(
+                this.coreInstances.Language.getText('ALERT_TITLE_ERROR_TEXT'),
+                this.coreInstances.Language.getText('CHAT_INPUT_SEND_MESSAGE_ERROR_TEXT'),
+                [
+                    { text: this.coreInstances.Language.getText('ALERT_BUTTON_TITLE_OK_TEXT'), onPress: () => { } }
+                ],
+                { cancelable: true }
+            );
+            return;
+        }
+
+        Image.getSize(`${this.coreInstances.linkConstants.LINK_API_URL_STICKER}${event.catId}/${event.name}`, (width, height) => {
+            let currentTime = new Date().getTime();
+            this.coreInstances.AZStackCore.newUniqueId();
+            let stickerMessage = {
+                chatType: this.props.chatType,
+                chatId: this.props.chatId,
+                senderId: this.coreInstances.AZStackCore.authenticatedUser.userId,
+                sender: this.coreInstances.AZStackCore.authenticatedUser,
+                receiverId: this.props.chatId,
+                receiver: this.props.chatTarget,
+                msgId: this.coreInstances.AZStackCore.uniqueId,
+                type: this.coreInstances.AZStackCore.chatConstants.MESSAGE_TYPE_STICKER,
+                status: this.coreInstances.AZStackCore.chatConstants.MESSAGE_STATUS_SENDING,
+                deleted: this.coreInstances.AZStackCore.chatConstants.MESSAGE_DELETED_FALSE,
+                created: currentTime,
+                modified: currentTime,
+                sticker: {
+                    name: event.name,
+                    catId: event.catId,
+                    url: `${this.coreInstances.linkConstants.LINK_API_URL_STICKER}${event.catId}/${event.name}`,
+                    width: width,
+                    height: height
+                }
+            };
+            this.props.onStickerMessageGenerated(stickerMessage);
+        }, (error) => {
+
+        });
     };
 
     render() {
@@ -98,7 +142,7 @@ class ChatInputStickerBlockComponent extends React.Component {
                                     <TouchableOpacity
                                         style={this.coreInstances.CustomStyle.getStyle('CHAT_INPUT_STICKER_BOX_STICKERS_STICKER_BUTTON_STYLE')}
                                         activeOpacity={0.5}
-                                        onPress={() => { this.props.onStickerPressed({ catId: this.state.items[this.state.selected].catId, name: item }) }}
+                                        onPress={() => { this.onStickerPressed({ catId: this.state.items[this.state.selected].catId, name: item }) }}
                                         key={`sticker_${this.state.items[this.state.selected].catId}_${item}`}
                                     >
                                         <Image
