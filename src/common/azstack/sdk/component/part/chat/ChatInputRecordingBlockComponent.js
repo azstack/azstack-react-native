a39f55d241c8f45baa0191d137cf2b207aa3bb5d
Dat Dang <datdh.js@gmail.com>
2018-02-21 11:19:06 +0700

    recording UI

--- src/common/azstack/sdk/component/part/chat/ChatInputRecordingBlockComponent.js
+++ src/common/azstack/sdk/component/part/chat/ChatInputRecordingBlockComponent.js
@@ -37,6 +37,51 @@ class ChatInputRecordingBlockComponent extends React.Component {
             <View
                 style={this.coreInstances.CustomStyle.getStyle('CHAT_INPUT_FILE_BOX_CONTENT_BLOCK_STYLE')}
             >
+                <View
+                    style={this.coreInstances.CustomStyle.getStyle('CHAT_INPUT_FILE_BOX_RECORDING_BLOCK_STYLE')}
+                >
+                    <View
+                        style={this.coreInstances.CustomStyle.getStyle('CHAT_INPUT_FILE_BOX_RECORDING_TIME_BLOCK_STYLE')}
+                    >
+                        <Text
+                            style={this.coreInstances.CustomStyle.getStyle('CHAT_INPUT_FILE_BOX_RECORDING_TIME_TEXT_STYLE')}
+                        >
+                            {'00:00'}
+                        </Text>
+                    </View>
+                    <View
+                        style={this.coreInstances.CustomStyle.getStyle('CHAT_INPUT_FILE_BOX_RECORDING_CONTROL_BLOCK_STYLE')}
+                    >
+                        <TouchableOpacity
+                            style={this.coreInstances.CustomStyle.getStyle('CHAT_INPUT_FILE_BOX_RECORDING_CONTROL_CANCEL_BUTTON_STYLE')}
+                            activeOpacity={0.5}
+                            onPress={() => { }}
+                        >
+                            <Text
+                                style={this.coreInstances.CustomStyle.getStyle('CHAT_INPUT_FILE_BOX_RECORDING_CONTROL_CANCEL_TEXT_STYLE')}
+                            >×</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            style={this.coreInstances.CustomStyle.getStyle('CHAT_INPUT_FILE_BOX_RECORDING_CONTROL_MAIN_BUTTON_STYLE')}
+                            activeOpacity={0.5}
+                            onPress={() => { }}
+                        >
+                            <Image
+                                style={this.coreInstances.CustomStyle.getStyle('CHAT_INPUT_FILE_BOX_RECORDING_CONTROL_MAIN_IMAGE_STYLE')}
+                                source={this.coreInstances.CustomStyle.getImage('IMAGE_VOICE')}
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            style={this.coreInstances.CustomStyle.getStyle('CHAT_INPUT_FILE_BOX_RECORDING_CONTROL_CONFIRM_BUTTON_STYLE')}
+                            activeOpacity={0.5}
+                            onPress={() => { }}
+                        >
+                            <Text
+                                style={this.coreInstances.CustomStyle.getStyle('CHAT_INPUT_FILE_BOX_RECORDING_CONTROL_CONFIRM_TEXT_STYLE')}
+                            >✓</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
                 <TouchableOpacity
                     style={this.coreInstances.CustomStyle.getStyle('CHAT_INPUT_FILE_BOX_CLOSE_BUTTON_BLOCK_STYLE')}
                     activeOpacity={0.5}
